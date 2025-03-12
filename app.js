81e2b9bc746c11851caf5ad0aee592bb8d817f75
tonmoy <tonmoy3673@gmail.com>
2025-03-12 21:21:11 +0600

    :bulb: make responsive and set dark mode

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('connected');
+// =========== implement dark mode =========//
+let icon = document.getElementById('img-mode');
+const changeMode=()=>{
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.src='/images/sun.png';
+    }
+    else{
+        icon.src='/images/moon.png'
+    }
+    
+}
